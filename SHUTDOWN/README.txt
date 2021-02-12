8d8ee9c8687edc35b5aba61571e86e8f391cc137
konsin1988 <konsin1988@gmail.com>
2021-02-12 15:57:17 +0300

    add SHUTDOWN

---
+++ SHUTDOWN/README.txt
@@ -0,0 +1,2 @@
+/* The program shuts down the computer without asking for sudo. 
+/* Three keys - and the computer is turned off))
---
+++ SHUTDOWN/main.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+	system("echo r13l02c1988 | sudo -S shutdown -h now");
+	return 0;
+}
